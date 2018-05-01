de95df758ae0d0b909d19fbfdfd6f53b17362736
Anton P. Linevich <anton@linevich.com>
2018-05-01 16:52:20 +0300

    install_requires

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='image2pipe',
-    version='0.0.9',
+    version='0.1.0',
     author='Anton P. Linevich',
     author_email='anton@linevich.com',
     keywords="ffmpeg yuv image2pipe",
@@ -12,6 +12,6 @@ setup(
     license='LICENSE.txt',
     description='Simple ffmpeg wrapper for image2pipe',
     long_description=open('README').read(),
-    install_requires=['tblib', 'numpy'],
+    install_requires=['tblib', 'numpy==1.14.2', 'opencv-python==3.4.0.12', 'websocket==0.2.1'],
     python_requires='>=2.6',
 )
